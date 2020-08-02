498f9e72d13856ac469050f5df3195cd864117bf
karol-nogal <karolnogal92@gmail.com>
2020-08-02 14:06:59 +0200

    Updates

---
+++ precache-manifest.ad3cda291242c36a83b2bdee8c6c0594.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "600b508e8fcb66644619565820fedcba",
+    "url": "/React-App/index.html"
+  },
+  {
+    "revision": "1319a64b876b4a761b7f",
+    "url": "/React-App/static/css/2.edb8eddf.chunk.css"
+  },
+  {
+    "revision": "3c2b29e94f566626e429",
+    "url": "/React-App/static/css/main.baf98cc7.chunk.css"
+  },
+  {
+    "revision": "1319a64b876b4a761b7f",
+    "url": "/React-App/static/js/2.c8ae7671.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React-App/static/js/2.c8ae7671.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c2b29e94f566626e429",
+    "url": "/React-App/static/js/main.b46c06d5.chunk.js"
+  },
+  {
+    "revision": "8af84ccf16105dfd53d5",
+    "url": "/React-App/static/js/runtime-main.2608efb7.js"
+  }
+]);
